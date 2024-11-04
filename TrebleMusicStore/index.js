44ab304adbdbf38bc8d4b50013783cc68b5d4593
CharZX1 <sonicadvanced21@hotmail.com>
2024-11-03 22:39:15 -0800

    Used JS to layout the basic structure for a music store site

---
+++ TrebleMusicStore/index.js
@@ -0,0 +1,28 @@
+const storeName = "Treble Clef";
+
+const storeType = "Music Store";
+
+const product1 = "Electric Guitar";
+const product2 = "Acoustic Guitar";
+const product3 = "Drum Set";
+const product4 = "Keyboard";
+const product5 = "Microphone";
+const product6 = "Amplifier";
+
+
+const price1 = 399
+const price2 = 200
+const price3 = 500
+const price4 = 300
+const price5 = 90
+const price6 = 150
+
+const storeHours = "Monday to Friday: 10AM - 8PM, Saturday: 10 AM - 6 PM, Sunday: Closed";
+
+console.log(storeName, storeType);
+console.log(product1, price1);
+console.log(product2, price2);
+console.log(product3, price3);
+console.log(product4, price4);
+console.log(product5, price5);
+console.log(storeHours);
